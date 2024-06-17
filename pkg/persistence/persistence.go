047bb74ed2270119da8a886be6b3f3e58ce7bd52
Bhanu Gupta <bhanu.mbvg@gmail.com>
2024-06-17 16:16:26 -0400

    chore: update .gitnore and persistence

--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -1,41 +1,40 @@
 package persistence
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "os"
+	"encoding/json"
+	"os"
 
-    "CollabDoc/pkg/document"
+	"CollabDoc/pkg/document"
 )
 
 type Persistence struct {
-    filePath string
+	filePath string
 }
 
 func NewPersistence(filePath string) *Persistence {
-    return &Persistence{filePath: filePath}
+	return &Persistence{filePath: filePath}
 }
 
 func (p *Persistence) SaveState(ss *document.StateSynchronizer) error {
-    data, err := json.Marshal(ss)
-    if err != nil {
-        return err
-    }
-    return ioutil.WriteFile(p.filePath, data, 0644)
+	data, err := json.Marshal(ss)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(p.filePath, data, 0644)
 }
 
 func (p *Persistence) LoadState() (*document.StateSynchronizer, error) {
-    data, err := ioutil.ReadFile(p.filePath)
-    if err != nil {
-        if os.IsNotExist(err) {
-            return document.NewStateSynchronizer(), nil
-        }
-        return nil, err
-    }
-    var ss document.StateSynchronizer
-    err = json.Unmarshal(data, &ss)
-    if err != nil {
-        return nil, err
-    }
-    return &ss, nil
+	data, err := os.ReadFile(p.filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return document.NewStateSynchronizer(), nil
+		}
+		return nil, err
+	}
+	var ss document.StateSynchronizer
+	err = json.Unmarshal(data, &ss)
+	if err != nil {
+		return nil, err
+	}
+	return &ss, nil
 }
